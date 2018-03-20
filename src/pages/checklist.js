94cad43268b1a166622565aebe443211dd94ed7a
Jin Xu <jin.xu@ef.com>
2018-03-20 18:16:05 +0800

    finish the main logic for check list page

--- src/pages/checklist.js
+++ src/pages/checklist.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Link from 'gatsby-link'
 
 import Header from '../components/Header/index'
 
 let todoList;
 const ls = window.localStorage;
-const initData = {
+let initData = {
     "todoList": [{ "name": "咽口水", "finished": false },
     { "name": "杯子", "finished": false },
     { "name": "毛毯", "finished": false },
@@ -26,28 +26,74 @@ const initData = {
     { "name": "中裤or裙子", "finished": false },
     { "name": "拖鞋", "finished": false },
     { "name": "运动鞋", "finished": false }]
+};
+
+// todo: remove these debug info
+initData = {
+    "todoList": [
+        { "name": "1", "finished": false },
+        { "name": "2", "finished": false },
+        { "name": "3", "finished": false },
+        { "name": "4", "finished": false }
+    ]
 }
 
 // try read list from ls
 var storedList = ls.getItem('todo');
 if (!storedList) {
     todoList = initData.todoList;
+    ls.setItem('todo', JSON.stringify(initData));
 }
 else {
     todoList = JSON.parse(storedList).todoList;
 }
 
-const Notify = () => (
-    <div>
-        <Header title="todo list" link="/" />
-        {/* <Header title="出行清单" link="/" /> */}
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-        <Link to="/">Go back to the homepage</Link>
-    </div>
-)
+class Notify extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoList: todoList
+        };
+    }
+
+    changeHandler = (idx) => {
+        // this.props.tabOnClick && this.props.tabOnClick(evt, value);
+        let resultList = [...todoList];
+        resultList[idx].finished = !resultList[idx].finished;
+        this.setState({ todoList: resultList });
+        ls.setItem('todo', JSON.stringify({ todoList: resultList }));
+    }
+
+    render() {
+
+        let list = todoList.map((li, idx) => {
+            return (
+                <li
+                    key={idx}
+                    onClick={() => this.changeHandler(idx)}
+                    style={{
+                        textDecoration: li.finished ? 'line-through' : 'none'
+                    }}
+                >
+                    {li.name}
+                </li>
+            );
+        });
+
+        return (
+            <div>
+                <Header title="todo list" link="/" />
+                {/* <Header title="出行清单" link="/" /> */}
+                <ul style={{
+                    listStyle: 'none'
+                }}>
+                    {list}
+                </ul>
+                <Link to="/">Go back to the homepage</Link>
+            </div>
+        );
+    }
+}
 
 export default Notify
